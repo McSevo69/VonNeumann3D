fbdd46561d9ce19095cdc1e8e93ecbfd1aade02f
McSevo69 <staudinger.sevi@gmail.com>
2017-06-17 02:09:54 +0200

    fix

--- GameOfLife_Sequentiell/Conway.c
+++ GameOfLife_Sequentiell/Conway.c
@@ -1,11 +1,13 @@
+#include <SDL2/SDL.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <time.h>
-#include "SDL2/SDL.h"
 
-#define SIZE 16384
-#define N 128
+#define SHAPE_SIZE 8
+#define SIZE 4096
+#define N 64
 #define IT 1000
 #define WINDOW_WIDTH 512
 
@@ -20,6 +22,13 @@ void init(int *a, int n, int alife) {
 		randomIndex = rand() % (n*n);
 		a[randomIndex] = 1;
 	}
+
+	//glider
+	//a[2] = 1;
+	//a[N] = 1;
+	//a[N+2] = 1;
+	//a[2*N+1] = 1;
+	//a[2*N+2] = 1;
 
 }
 
@@ -57,12 +66,22 @@ void golAlgorithm(int *dataIn, int *dataOut) {
 	
 			//rules for surviving
 			switch(dataIn[j*N+k]) {
-				case 0: if (neighbors==3) dataOut[j*N+k]=1; break;
+				case 0: if (neighbors==3) {
+							dataOut[j*N+k]=1; 
+							break;
+						} else {
+							dataOut[j*N+k]=0;
+							break;
+						}
 				case 1:
 					//loneliness
-					if (neighbors < 2) dataOut[j*N+k]=0; break;
-					//overpopulation
-					if (neighbors > 3) dataOut[j*N+k]=0; break;
+					if (( neighbors < 2) || (neighbors > 3)) {
+						dataOut[j*N+k]=0; 
+						break;
+					} else {
+						dataOut[j*N+k]=1;
+						break;
+					}
 			}				
 
 		}
@@ -70,107 +89,125 @@ void golAlgorithm(int *dataIn, int *dataOut) {
 
 }
 
-void quit(int rc) {
-	
-	SDL_Quit();
-	exit(rc);
-}
 
-void drawResults(SDL_Renderer *rend, int *dataToPlot) {
+int main(int argc, char *argv[])
+{
 
-	for (int i=0; i<N; i++) {
-		for (int j=0; j<N; j++) {
-			if (dataToPlot[i*N+j] == 1) {
-				for (int a=0; a<4; a++) {
-					for (int b=0; b<4; b++) {
-						SDL_RenderDrawPoint(rend, 4*i-a, 4*j-b);
-					}
-				}
-				//SDL_RenderDrawPoint(rend, i, j);
-			}
-	
-		}
-	}
-		
-	SDL_RenderPresent(rend);
-
-
-}
-
-
-int main( int argc, char* args[] ) {
-
-	//SDL		
-	SDL_Renderer *renderer;
-	SDL_Window *window;
-
-	/* Create the window and renderer */
-    window = SDL_CreateWindow("Game Of Life",
-							SDL_WINDOWPOS_UNDEFINED,
-                            SDL_WINDOWPOS_UNDEFINED,
-							N*4, N*4,
-							SDL_WINDOW_RESIZABLE);
-
-	renderer = SDL_CreateRenderer(window, -1, 0);
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-	SDL_RenderClear(renderer);
-	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-
-
-	int * dataIn = malloc(SIZE*sizeof(int));
+	int * dataIn = calloc(SIZE, sizeof(int));
 	int ** dataOut = calloc(IT*SIZE, sizeof(int*));
 	for (int i=0; i<IT; i++) dataOut[i] = calloc(SIZE, sizeof(int));
 
-	init(dataIn, N, (N*N-N/4));
+	init(dataIn, N, SIZE-N/8);
 
 	clock_t start = clock();
 
 	//first iteration
-	printf("Iteration 1\n");
+	//printf("Iteration 1\n");
 	golAlgorithm(dataIn,dataOut[0]);
 
 	for (int it_cnt = 1; it_cnt < IT; it_cnt++) {
-		printf("Iteration %d\n" , it_cnt+1);
+		//printf("Iteration %d\n" , it_cnt+1);
 		golAlgorithm(dataOut[it_cnt-1],dataOut[it_cnt]);
 	}
 
 	clock_t end = clock();
 	double timeSpent = (end-start)/(double)CLOCKS_PER_SEC;
 	printf("time: %.2f sec\n",timeSpent);
-	printf("results are stored.\n");
 
-	FILE* results;
-	results = fopen("results_seq_new.csv","a");
+  SDL_Window* Main_Window;
+  SDL_Renderer* Main_Renderer;
+  SDL_Surface* Loading_Surf;
+  SDL_Texture* Background_Tx;
+  SDL_Texture* WhiteCell;
 
-	for (int j = 1; j < IT; j++) {
+  /* Rectangles for drawing which will specify source (inside the texture)
+  and target (on the screen) for rendering our textures. */
+  SDL_Rect SrcR;
+  SDL_Rect DestR;
 
-		for(int i = 0; i < SIZE; ++i){
-			fprintf(results, "%d,", dataOut[j][i]);
+  SrcR.x = 0;
+  SrcR.y = 0;
+  SrcR.w = SHAPE_SIZE;
+  SrcR.h = SHAPE_SIZE;
+
+  DestR.x = SHAPE_SIZE / 2 - SHAPE_SIZE / 2;
+  DestR.y = SHAPE_SIZE / 2 - SHAPE_SIZE / 2;
+  DestR.w = SHAPE_SIZE;
+  DestR.h = SHAPE_SIZE;
+
+
+  /* Before we can render anything, we need a window and a renderer */
+  Main_Window = SDL_CreateWindow("SDL_RenderCopy Example",
+  SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 512, 512, 0);
+  Main_Renderer = SDL_CreateRenderer(Main_Window, -1, SDL_RENDERER_ACCELERATED);
+
+  /* The loading of the background texture. Since SDL_LoadBMP() returns
+  a surface, we convert it to a texture afterwards for fast accelerated
+  blitting. */
+  Loading_Surf = SDL_LoadBMP("background.bmp");
+  Background_Tx = SDL_CreateTextureFromSurface(Main_Renderer, Loading_Surf);
+  SDL_FreeSurface(Loading_Surf); /* we got the texture now -> free surface */
+
+  /* Load an additional texture */
+  Loading_Surf = SDL_LoadBMP("white.bmp");
+  WhiteCell = SDL_CreateTextureFromSurface(Main_Renderer, Loading_Surf);
+  SDL_FreeSurface(Loading_Surf);
+
+  /* now onto the fun part.
+  This will render a rotating selection of the blue shapes
+  in the middle of the screen */
+  int i;
+  //int n;
+  for (i = 0; i < IT; ++i) {
+
+      /* render background, whereas NULL for source and destination
+      rectangles just means "use the default" */
+      SDL_RenderCopy(Main_Renderer, Background_Tx, NULL, NULL);
+
+      /* render the current animation step of our shape */
+      //SDL_RenderCopy(Main_Renderer, BlueShapes, &SrcR, &DestR);
+
+	  for (int j=0; j<N; j++) {
+		for (int k=0; k<N; k++) {
+			if (dataOut[i][j*N+k]==1) {
+      			DestR.x = SHAPE_SIZE*2*(k) / 2;
+  	  			DestR.y = SHAPE_SIZE*2*(j) / 2;
+		    	SDL_RenderCopy(Main_Renderer, WhiteCell, &SrcR, &DestR);
+			}
 		}
+	  }
 
-		fprintf(results, "\n");
+      SDL_RenderPresent(Main_Renderer);
+      SDL_Delay(300);
+    
+  }
 
-	}
 
-	fclose (results);
-	printf("done.");
-	
+  /* The renderer works pretty much like a big canvas:
+  when you RenderCopy() you are adding paint, each time adding it
+  on top.
+  You can change how it blends with the stuff that
+  the new data goes over.
+  When your 'picture' is complete, you show it
+  by using SDL_RenderPresent(). */
+
+  /* SDL 1.2 hint: If you're stuck on the whole renderer idea coming
+  from 1.2 surfaces and blitting, think of the renderer as your
+  main surface, and SDL_RenderCopy() as the blit function to that main
+  surface, with SDL_RenderPresent() as the old SDL_Flip() function.*/
+
+  SDL_DestroyTexture(WhiteCell);
+  SDL_DestroyTexture(Background_Tx);
+  SDL_DestroyRenderer(Main_Renderer);
+  SDL_DestroyWindow(Main_Window);
+  SDL_Quit();
+
+
 	free(dataIn);
-
-
-	for (int j = 0; j < IT; j++) {
-		drawResults(renderer, dataOut[j]);
-		free(dataOut[j]);
-		SDL_Delay(400);
-	}
-
+	for (int i=0; i<IT; i++) free(dataOut[i]);
 	free(dataOut);
 
-	//Quit SDL
-	SDL_DestroyRenderer(renderer);
- 	SDL_DestroyWindow(window);
 
-	quit(0);
-
+  return 0;
 }
 
